2c6b65a33a6c686e8bcdc82b2eee0e896fbcbc71
sandipan898 <sandipan.das898@gmail.com>
2021-01-10 14:16:19 +0530

    creating static and template folder

---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal Website
+
+## Sections:
+1.   Article and Blogs
+2.   QnA and discussion Forum
+3.   Portfolio
--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Article
 
 # Create your views here.
+
+
