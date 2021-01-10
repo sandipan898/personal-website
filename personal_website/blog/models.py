06515bc68db916edafbb8f82eecff80e5f3045f5
sandipan898 <sandipan.das898@gmail.com>
2021-01-10 13:39:58 +0530

    crating Article model

--- personal_website/blog/models.py
+++ personal_website/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.related import OneToOneField
 
 # Create your models here.
+
+class Article(models.Model):
+    author = OneToOneField(User, on_delete=models.CASCADE)
+    topic_related_to = models.CharField(max_length=400, blank=True, null=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True, null=True)
+    
