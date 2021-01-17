044a0a4a34ffc1c84404c9fdab6dde1075dbe875
sandipan898 <sandipan.das898@gmail.com>
2021-01-17 11:37:22 +0530

    adding templates

--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -18,18 +18,11 @@ def home_view(request):
     
 def article_list_view(request):
     template_name = 'blog/article-list.html'
-    return render(request, template_name=template_name)
-    
-# def post_detail_view(request, id):
-#     print(id)
-#     template_name = 'blog/detail-page.html'
-#     return render(request, template_name=template_name)
-    
+    return render(request, template_name=template_name)  
+  
 
 class PostDetailView(DetailView):
     model = Article
     template_name = 'blog/detail-page.html'
-
-
 post_detail_view = PostDetailView.as_view()
     
