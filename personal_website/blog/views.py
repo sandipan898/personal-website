833e262e9c066aecaaa83bc8a3229764c26d02d0
sandipan898 <sandipan.das898@gmail.com>
2021-01-17 16:43:28 +0530

    making templates

--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -9,3 +9,12 @@ def home_view(request):
     template_name = 'blog/home.html'
     return render(request, template_name=template_name)
     
+
+def article_list_view(request):
+    template_name = 'blog/article-list.html'
+    return render(request, template_name=template_name)
+    
+def post_detail_view(request):
+    template_name = 'blog/detail-page.html'
+    return render(request, template_name=template_name)
+    
