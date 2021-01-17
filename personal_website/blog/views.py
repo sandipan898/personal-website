657f6631bb4a30a9e9bf82695c69c40019b4a3a4
sandipan898 <sandipan.das898@gmail.com>
2021-01-17 07:24:03 +0530

    Modifying templates

--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -9,7 +9,6 @@ def home_view(request):
     template_name = 'blog/home.html'
     return render(request, template_name=template_name)
     
-
 def article_list_view(request):
     template_name = 'blog/article-list.html'
     return render(request, template_name=template_name)
