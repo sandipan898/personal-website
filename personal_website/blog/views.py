6c88006add91eed075e4f1912188a32570732e7c
sandipan898 <sandipan.das898@gmail.com>
2021-01-17 07:42:01 +0530

    Modifying templates

--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -7,7 +7,12 @@ from .models import Article
 
 def home_view(request):
     template_name = 'blog/home.html'
-    return render(request, template_name=template_name)
+    articles = Article.objects.all()
+
+    context = {
+        "articles": articles
+    }
+    return render(request, context=context, template_name=template_name)
     
 def article_list_view(request):
     template_name = 'blog/article-list.html'
