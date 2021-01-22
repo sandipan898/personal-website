4ccf089dddcb17ea5a5259d667052ebf6275dd2f
sandipan898 <sandipan.das898@gmail.com>
2021-01-23 00:26:51 +0530

    adding changes

--- personal_website/blog/urls.py
+++ personal_website/blog/urls.py
@@ -3,6 +3,6 @@ from .views import home_view, article_list_view, post_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('articles/', article_list_view, name='article-list'),
-    path('articles/detail/', post_detail_view, name='article-detail'),
+    path('article/all/', article_list_view, name='article-list'),
+    path('article/<int:id>/detail/', post_detail_view, name='article-detail'),
 ]
