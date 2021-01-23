3e2e0fd2a3beed603ad300ba078c8c59570b0ca6
sandipan898 <sandipan.das898@gmail.com>
2021-01-24 00:40:25 +0530

    creating user signup form

--- personal_website/blog/forms.py
+++ personal_website/blog/forms.py
@@ -1,14 +1,22 @@
 from django import forms 
 from ckeditor.fields import RichTextField
 from ckeditor.widgets import CKEditorWidget
-from .models import Article
+from .models import Article, ArticleUser
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# class ArticlePostForm(forms.Form):
-#     topic_related_to = forms.CharField(max_length=400)
-#     title = forms.CharField(max_length=200)
-#     content = RichTextField()
-#     tags = forms.CharField(max_length=100)
-#     thumbnail = forms.ImageField()
+
+class SignUpUserForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100, help_text='First Name')
+    last_name = forms.CharField(max_length=100, help_text='Last Name')
+    email = forms.EmailField(max_length=150, help_text='Email')
+    bio = forms.CharField(max_length=400, help_text='Bio')
+    image = forms.ImageField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'bio', 'password1', 'password2', 'image')
+
 
 class ArticlePostForm(forms.ModelForm):
     title = forms.CharField(
--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -3,9 +3,10 @@ from django.views import generic
 from .models import Article
 from django.views.generic import DetailView
 from django.urls import reverse
-from .forms import ArticlePostForm
+from .forms import ArticlePostForm, SignUpUserForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
@@ -30,7 +31,6 @@ def article_list_view(request):
   
 def post_detail_view(request, slug):
     template_name = 'blog/detail-page.html'
-    # queryset = Article.objects.filter(slug=slug)
     selected_article = get_object_or_404(Article, slug=slug)
     related_articles = Article.objects.all()
     return render(request, context={'article': selected_article, 'related_articles': related_articles}, template_name=template_name)
@@ -61,3 +61,24 @@ def create_article_view(request):
         context['form'] = form
     
     return render(request, template_name, context=context)
+
+
+def signup_view(request):
+    form = SignUpUserForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        user.refresh_from_db()
+        user.profile.first_name = form.cleaned_data.get('first_name')
+        user.profile.last_name = form.cleaned_data.get('last_name')
+        user.profile.email = form.cleaned_data.get('email')
+        user.profile.email = form.cleaned_data.get('bio')
+        user.profile.email = form.cleaned_data.get('image')
+        user.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+    else:
+        form = SignUpUserForm()
+    return render(request, 'signup.html', {'form': form})
