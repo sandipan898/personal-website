0cf7f348d73547d192f17eacb8a87ba1effb97e9
sandipan898 <sandipan.das898@gmail.com>
2021-01-27 01:41:58 +0530

    adding portfolio templates

--- personal_website/portfolio/forms.py
+++ personal_website/portfolio/forms.py
@@ -2,13 +2,37 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(required=True, label='Your name',max_length=100)
+    name = forms.CharField(required=True, label='Your name',max_length=100,
+                widget=forms.TextInput(
+                    attrs={
+                        'class': 'form-control'
+                        }
+                )
+)
 
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=False, max_length=100)
-    message = forms.CharField(widget=forms.Textarea(
-                attrs={
-                        'rows': 5,
-                }
+    from_email = forms.EmailField(
+            required=True,
+            widget=forms.EmailInput(
+                    attrs={
+                        'class': 'form-control'
+                        }
+                )
+
         )
-    )
+    subject = forms.CharField(
+            required=False, 
+            max_length=100,
+            widget=forms.TextInput(
+                    attrs={
+                        'class': 'form-control'
+                        }
+                )
+        )
+    message = forms.CharField(
+            widget=forms.Textarea(
+                    attrs={
+                        'rows': 5,
+                        'class': 'form-control'
+                        }
+                )
+        )
