bf687c7a7254f9bb9c21d028142cbac4905ff3fe
sandipan898 <sandipan.das898@gmail.com>
2021-01-27 00:24:11 +0530

    Creating portfolio app

--- personal_website/personal_website/urls.py
+++ personal_website/personal_website/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('user/', include('authuser.urls')),
-    
+    path('portfolio/', include('portfolio.urls')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
