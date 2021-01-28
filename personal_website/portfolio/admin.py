5359e29d2c1d76a1ed2dc88502fd39b434674ec9
sandipan898 <sandipan.das898@gmail.com>
2021-01-28 23:59:04 +0530

    updating portfolio models

--- personal_website/portfolio/admin.py
+++ personal_website/portfolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Project, Hobby, Achievement, Skill
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Hobby)
+admin.site.register(Achievement)
+admin.site.register(Skill)
