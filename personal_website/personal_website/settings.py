02d9360be7d34e9735e9c78f6beaae4b8126d9a7
sandipan898 <sandipan.das898@gmail.com>
2021-02-10 13:04:23 +0530

    updating settings to deploy

--- personal_website/personal_website/settings.py
+++ personal_website/personal_website/settings.py
@@ -23,7 +23,9 @@ PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$o#k)@_gar7d4r1vin7-=n3zzw=5$r2g=wkou(mkrk_lxi*lvd'
+# SECRET_KEY = '$o#k)@_gar7d4r1vin7-=n3zzw=5$r2g=wkou(mkrk_lxi*lvd'
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
