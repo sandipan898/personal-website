81ab15574ae0eeba7229be66c7c850aaeb12d702
sandipan898 <sandipan.das898@gmail.com>
2021-04-09 00:47:42 +0530

    updating frontend

--- personal_website/code_arena/models.py
+++ personal_website/code_arena/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Code(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    instructions = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- personal_website/code_arena/views.py
+++ personal_website/code_arena/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Code
 
 # Create your views here.
+
+
+class HomeView(View):
+    template_name = ""
+    def get(self, request, *args, **kwargs):
+        render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        pass
+
