f0faf2ba13c4e32cdccf40b76849907b3ec8847b
sandipan898 <sandipan.das898@gmail.com>
2021-04-14 14:46:24 +0530

    Creating FAQ models

--- personal_website/qna/models.py
+++ personal_website/qna/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class FAQ(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.TextField(blank=True, null=True)
+    answer = models.TextField(blank=True, null=True)
+    is_answered = models.BooleanField( , blank=True, null=True)
+    upvotes = models.IntegerField(default=0, blank=True, null=True)
+    downvotes = models.IntegerField(default=0, blank=True, null=True)
+
+    def __str__(self):
+        return self.question
