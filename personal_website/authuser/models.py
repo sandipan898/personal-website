0bd435f9df9c8b6773910272cf305fcd9bfc2b35
sandipan898 <sandipan.das898@gmail.com>
2021-05-24 01:39:56 +0530

    updating styles

--- personal_website/authuser/models.py
+++ personal_website/authuser/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
+    bio = models.CharField(max_length=2000, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
